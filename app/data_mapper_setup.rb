4138794e0975d48563cad727663501e4a47fd61b
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-30 14:28:48 +0100

    setting up database for heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,7 +4,7 @@ require './app/models/tag'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 DataMapper.finalize
 
