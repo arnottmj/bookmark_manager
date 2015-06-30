c61b1982320d6bb54bcf2a8111a4b645ed7fab5d
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-30 11:21:41 +0100

    Creating tag model and making sure the tag database is written to when filling out form

--- app/app.rb
+++ app/app.rb
@@ -12,7 +12,10 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.new(url: params[:url], title: params[:title])
+    tag = Tag.create(text: params[:tag])
+    link.tags << tag
+    link.save
     redirect to('/links')
   end
 
