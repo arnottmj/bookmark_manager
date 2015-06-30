2d1a70cb3d74523fc40af34bd9996e51380d114e
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-30 17:58:17 +0100

    Password Confirmation

--- app/app.rb
+++ app/app.rb
@@ -7,7 +7,7 @@ class BookmarkManager < Sinatra::Base
   set :session_secret, 'super secret'
 
   get '/' do
-    redirect '/links'
+    redirect '/users/new'
   end
 
   get '/links' do
@@ -42,7 +42,8 @@ class BookmarkManager < Sinatra::Base
 
   post '/users' do
     user = User.create(email: params[:email],
-                password: params[:password])
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
     redirect to('/links')
   end
